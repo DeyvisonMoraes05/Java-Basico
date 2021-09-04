88fb2867755ab055da5ce543165f47152f3fc20f
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-04 10:39:01 -0400

    Questões Teóricas de Java
    Aula 1

---
+++ Aula01ExerciciosQuestoesTeoricas/src/aula01exerciciosquestoesteoricas/Aula01ExerciciosQuestoesTeoricas.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula01exerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula01ExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* QUESTÃO 1
+        Assinale a alternativa que contém a descrição correta de algoritimo
+        
+        a) Algoritmo é uma coleção de livros de uma mesma matéria,
+        normalmente relacionada à Engenharia de Software.
+        
+        b)Algotitmo é uma operação matemática usada, por exemplo,
+        pata calcular a intensidade sonora de decibéis.
+        
+        c) Algoritmo é uma descrição das etapas de resolução de um
+        problema ou a indicação ordenada de uma sequência de ações
+        bem-definidas. // RESPOSTA CERTA
+        
+        d) Algoritmo é uma definição formal da hierarquia de funcio -
+        nários de uma empresa de desenvolvimento de software de grande porte.
+        */
+        
+        /* QUESTÃO 2
+        Assinale a opção que não apresenta um exemplo de Linguagem de Programação.
+        
+        (A) Delphi.
+        (B) De Máquina.
+        (C) Aseembly.
+        (D) Windows XP. // RESPOSTA CERTA
+        (E) Visual Basic.
+        */
+        
+        
+        /* QUESTÃO 3
+        Qual é o utilitário responsável por gerar, a partir de um programa 
+        escrito em uma liunguagem de alto nível, um programa em linguagem
+        de máquina não executável chamado de módulo-objeto?
+        
+        (A) Interpretador. 
+        (B) Depurador.
+        (C) Loader.
+        (D) Linker. 
+        (E) Compilador. // RESPOSTA CERTA
+        */
+        
+        /* QUESTÃO 4
+        A expressão Z = X % Y, corresponde a Z igual 
+        
+        a) a X % do valor de Y.
+        b) ao resto da divisão de Y por X.
+        c) ao resto da divisão de X por Y. // RESPOSTA CERTA
+        d) ao quociente da divisão de X por Y.
+        e) ao quociente da divisão de Y por X.
+        */
+        
+        /* QUESTÃO 5
+        Fluxograma é um tipo de
+        
+        a) representação gráfica de algoritmos. // RESPOSTA CERTA
+        b) informações sobre tipos de dados.
+        c) livro de análise de sistemas. 
+        d) análise de sistemas.
+        */
+        
+        /* QUESTÃO 6
+        Assinale a alternativa que contém o símbolo utilizado em
+        fluxogramas de sistemas de processamento de dados para indicar
+        saídas impressas como, por exemplo, a emissão de um ou vários relatórios
+        
+        // RESPOSTA CERTA LETRA C
+        */
+        
+        /* QUESTÃO 7
+        Relacione os símbolos utilizados no fluxograma com seus
+        respectivos nome e, a seguir, assinale a alternativa que
+        contém a sequência correta.
+        
+        1 - Terminal
+        
+        2 - Seta de fluxo de dados
+        
+        3 - Processamento
+        
+        4 - entrada de dados ou operação manual
+        
+        5 - Entrada e saída de dados
+        
+        6 - Saída de dados em vídeo
+        
+        7 - Sapida de dados em impressora
+        
+        8 - Decisão
+        
+        a) 3 - 1 - 6 - 4 - 7 - 5 - 8 - 2 // RESPOSTA CERTA
+        b) 3 - 1 - 6 - 5 - 7 - 4 - 8 - 2
+        c) 1 - 3 - 4 - 6 - 5 - 7 - 2 - 8
+        d) 1 - 3 - 6 - 4 - 7 - 5 - 8 - 2
+        */
+        
+    }
+    
+}
