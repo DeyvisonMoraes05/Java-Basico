16b789ca1eadeeac550c2e148055a6548652d84c
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-04 11:01:20 -0400

    Questões Teóricas de Java
    Aula 2

---
+++ Aula02ExerciciosQuestoesTeoricas/src/aula02exerciciosquestoesteoricas/Aula02ExerciciosQuestoesTeoricas.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02exerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula02ExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* QUESTÃO 1
+        Em relação à linguagem de programação Java, complete corretamente
+        as lacunas das sentenças abaixo, e assinale a opção correta.
+        
+        I - O comando _____ do J2SE Development Kit executa um aplicativo Java.
+        
+        II - O comando _____ do J2SE Development Kit compila um aplicativo Java.
+        
+        III - Um arquivo de programa Java deve terminar com a extensão de 
+        arquivo _____.
+        
+        IV - Quando um programa Java é compilado, o arquivo produzido pelo com
+        pilador termina com a estensão de arquivo _____.
+        
+        V - O arquivo produzido pelo compilador Java contém _____ que são execu
+        tados pela Java Virtual Machine.
+        
+        
+        (A) java / javac / java / class / bytecodes. // RESPOSTA CERTA
+        (B) javac / java / class / java / bytecodes.
+        (C) main / javac / java / jar / class.
+        (D) java / class / rar / jar / javac.  
+        (E) rar / jar / java / javac / class. 
+        */
+        
+        /* QUESTÃO 2
+        Coloque Falsas (F) ou Verdadeirras (V) e em seguida assinale a opção
+        que contém a sequência correta.
+        
+        () A área de memória requirida para operar com o programa compilado
+        é menor que a requerida para a interpretação.
+        
+        () Um programa interpretado requer uma área de memória menor do que
+        a do programa compilado.
+        
+        () O tempo para execução interpretada é maior do que o tempo para a
+        execução compilada.
+        
+        () Um programa compilado requer um tempo de execução maior do que
+        um programa interpretado.
+        
+        a) F - V - V - F // RESPOSTA CERTA  c) F - V - V - V
+        b) V - F - F - V                    d) V - F - V - V
+        
+        "TESTE" DE MESA
+        COMPILADOR  | INTERPRETADOR
+        + MEMÓRIA   | - MEMÓRIA
+        - TEMPO     | + TEMPO
+        */
+        
+        
+        /* QUESTÃO 3
+        Na construção de um algoritmo, como seria
+        representado o cálculo da multiplicação da base
+        pela altura e em seguida a divisão pela
+        constante 2?
+        
+        a) área = Base • Altura 2
+        b) área <- (base * altura) / 2 // RESPOSTA CERTA
+        c) área <- base • altura / 2
+        d) área = base * altura / 2
+        */
+        
+        
+        /* QUESTÃO 4
+        Assinale a alternativa que corresponde à fórmula correta para
+        calcular a média aritmética entre 4 notas, representadas pelas
+        variáveis N1, N2, N3, N4.
+        
+        a) N1+N2+N3+N4/4
+        b) N1+N2+N3+(N4/4)
+        c) N.(1+2+3+4)/4
+        d) (N1+N2+N3+N4)/4 // RESPOSTA CERTA
+        */
+        
+        /* QUESTÃO 5
+        Informe se é falso (F) ou verdadeiro (V) o que se afirma sobre
+        o diagrama de blocos abaixo. A seguir, indique a opção com a 
+        sequência correta.
+        
+        Considere:
+        C1 e C2 são condições.
+        L1, L2, L3 e L4 são instruções.
+        
+        () L1 executa se C1 for falsa.
+        () L2 somente executa  se C2 for verdadeira.
+        () Se C1 e C2 forem falsas a L3 executa.
+        () L4 sempre executa, independente de C1 e C2.
+        
+        a) V - F - V - V   
+        b) F - F - F - V
+        c) V - F - V - F // RESPOSTA CERTA
+        d) f - V - V - F
+        */
+    }
+    
+}
