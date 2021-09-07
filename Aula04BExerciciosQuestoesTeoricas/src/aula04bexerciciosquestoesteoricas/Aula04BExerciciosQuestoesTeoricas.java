925f818eb10a672c7e5f43e059596e29ea0a0fc8
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-06 22:48:03 -0400

    Aula 4 B

---
+++ Aula04BExerciciosQuestoesTeoricas/src/aula04bexerciciosquestoesteoricas/Aula04BExerciciosQuestoesTeoricas.java
@@ -0,0 +1,75 @@
+package aula04bexerciciosquestoesteoricas;
+
+public class Aula04BExerciciosQuestoesTeoricas {
+
+    public static void main(String[] args) {
+        /* QUESTÃO 1
+        Indique a opção que contém o valor final da variável X,
+        após a execução do trecho de programa em Português
+        Estruturado mostrado ao lado. Considere os seguintes
+        valores para as variáveis: A = 3; B = 2; C = 8; D = 7.
+        
+        se .não. (A > 3) .e. .não. (B < 5) então
+            X <- 10
+        senão
+            se (A >= 2) .ou. (C <= 1) então
+                X <- (A + D) / 2
+            senão
+                se (A = 2) .ou. (B < 7) então
+                    X <- (A + 2) * (B – 2)
+                senão
+                    X <- ((A + C) / B * (C + D)
+                fim_se
+            fim_se
+        fim_se
+        
+        
+        a) 10
+        b) 5 // RESPOSTA CERTA
+        c) zero
+        d) 82,5
+        
+        "TESTE DE MESA"
+        A = 3; B = 2; C = 8; D = 7.
+        se .não. (A > 3) .e. .não. (B < 5) então
+        SE (V) .E. (F) = (F)
+            X <- 10
+            X = 10
+        senão
+            se (A >= 2) .ou. (C <= 1) então
+                (V) .OU. (F) = V
+                X <- (A + D) / 2
+                X = (3 + 7) / 2
+                X = 5
+            senão
+                se (A = 2) .ou. (B < 7) então
+                    (F) .OU. (V) = (V)
+                    X <- (A + 2) * (B – 2)
+                    X = (3 + 2) * (2 - 2)
+                    X = 5
+                senão
+                    X <- ((A + C) / B * (C + D)
+                    X = (3 + 8) / 2 * (8 + 7)
+                    X = 11 / 2 * 15
+                    X = 
+                fim_se
+            fim_se
+        fim_se
+        
+        */
+        
+        /* QUESTÃO 2
+        Indique a alternativa que tem a representação na
+        forma da expressão aritmética abaixo:
+        
+        Z=52 ÷ (32–13)+8.2
+        
+        a) Z=5∗5/(32-13)+8×2
+        b) Z=←(5∗5/(32-13)+(8*2)
+        c) Z=5↑2/(32-13)+8×2
+        d) Z←((5 ↑ 2)/(32-13)+(8∗2)) // RESPOSTA CERTA
+
+        */
+    }
+    
+}
