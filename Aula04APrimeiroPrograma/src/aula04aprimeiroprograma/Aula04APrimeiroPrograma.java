48db6ef5463dbdc02bcb9e3a3a9352ad54074b9e
Deyvison Thiago Gomes de Moraes <75688435+DeyvisonMoraes05@users.noreply.github.com>
2021-09-06 22:56:23 -0400

    Aula 4A: Primeiro Programa em Java
    Olá, Mundo!

--- Aula04APrimeiroPrograma/src/aula04aprimeiroprograma/Aula04APrimeiroPrograma.java
+++ Aula04APrimeiroPrograma/src/aula04aprimeiroprograma/Aula04APrimeiroPrograma.java
@@ -15,7 +15,6 @@ public class Aula04APrimeiroPrograma {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Ola, Mundo!");
     }
     
