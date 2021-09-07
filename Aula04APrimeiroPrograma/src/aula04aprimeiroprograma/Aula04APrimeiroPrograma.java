8f4e8e1e8bdb3acb3466bb012cb43bdc329df609
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-06 22:41:26 -0400

    Primeiro Programa em Java

---
+++ Aula04APrimeiroPrograma/src/aula04aprimeiroprograma/Aula04APrimeiroPrograma.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula04aprimeiroprograma;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula04APrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ola, Mundo!");
+    }
+    
+}
