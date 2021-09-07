95fd43e803f5dc14bc0f2493697d440a89f35041
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-06 23:08:53 -0400

    Aula 4C

---
+++ Aula04CExercicioHoraDoSistema/src/aula04cexerciciohoradosistema/Aula04CExercicioHoraDoSistema.java
@@ -0,0 +1,26 @@
+package aula04cexerciciohoradosistema;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+public class Aula04CExercicioHoraDoSistema {
+
+    public static void main(String[] args) {
+        /*
+        Date data = new Date(); // CRIANDO UM NOVO OBJETO
+        System.out.println("A hora do sistema é ");
+        System.out.println(data.toString());*/
+        
+        Calendar ano = GregorianCalendar.getInstance();
+        //Calendar ano = GregorianCalendar.getInstance();
+        Date horas = new Date();
+        //Date horas = new Date();
+        System.out.println("Horas: "+horas);
+        //System.out.println("Horas: " + horas);
+        System.out.println("Ano Atual: "+ano.get(Calendar.YEAR));
+        //System.out.println("Ano Atual: " + ano.get(Calendar.YEAR));
+    }
+    
+}
