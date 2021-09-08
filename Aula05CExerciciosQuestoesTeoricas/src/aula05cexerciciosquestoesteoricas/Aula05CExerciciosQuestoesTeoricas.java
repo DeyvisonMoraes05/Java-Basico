dfb505c4700e1e527a0be155752e3c242b907523
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-07 22:46:37 -0400

    Aula 5C

---
+++ Aula05CExerciciosQuestoesTeoricas/src/aula05cexerciciosquestoesteoricas/Aula05CExerciciosQuestoesTeoricas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05cexerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula05CExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* QUESTÃO 1
+        Segundo a Linguagem de Programação Java, assinale a opção INCORRETA.
+        
+        (A) O java diferencia letras maiúsculas e minúsculas.
+        (B) A palavra-chave NEW introduz uma declaração de classe e 
+        é imediatamente seguida pelo nome da classe. // RESPOSTA CERTA
+        (C) O método MAIN é o ponto de partida de cada aplicativo java e deve
+        iniciar com public static void main(String args[] ).
+        (D) O corpo de cada declaração de classe é delimitado por chaves.
+        (E) Cada instrução termina com um ponto e vírgula 
+        */
+        
+        /* QUESTÃO 2
+        Uma pilha é uma versão limitada de uma lista encadeada. Novos nós só 
+        podem ser adicionados e retirados de uma pilha pelo topo. Em JAVA, a
+        classe STACK possui métodos de manipulação em pilhas. Em qual pacote 
+        a classe STACK definida?
+        
+        (A) java.util // RESPOSTA CERTA
+        (B) java.lang
+        (C) java.net
+        (D) java.io
+        (E) java.swt
+        */
+        
+        /* QUESTÃO 3
+        Na linguagem de programação JAVA, que palavra-chave cria um
+        obejto de uma classe específica
+        
+        (A) MAIN 
+        (B) PUBLIC
+        (C) CLASS
+        (D) PROVATE
+        (E) NEW // RESPOSTA CERTA
+        */
+    }
+    
+}
