e12bbd86ffa6f0f8d69cab842d676183b897f422
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-08 23:06:48 -0400

    Aula 6D

---
+++ Aula06DExerciciosQuestoesTeoricas/src/aula6questoes/Aula6Questoes.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula6questoes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula6Questoes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* QUESTÃO 1
+        Em relação aos tipos básicos de dados (tipos primitvos),
+        assinale a opção INCORRETA.
+        
+        (A) boolean (ou lógico): conjunto de valores falso ou verdadeiro.
+        (B) vetor: estrutura que suporta NXM posições de um mesmo tipo. // RESPOSTA CERTA
+        (C) caracter: qualquer conjunto de caracteres alfanuméricos.
+        (D) inteiro: qualquer número inteiro, negativo, nulo ou positivo.
+        (E) real: qualquer número real, negativo, nulo ou positivo 
+        */
+        
+        /* QUESTÃO 2
+        Marque 1 para dadis do tipo inteiro e 2 para dados de outros tipos.
+        
+        () "582.4"
+        () .verdadeiro
+        () 105
+        () -102
+        () "0"
+        () "informação"
+        () 0.82
+        
+        a) 2,2,1,1,1,2,1 
+        b) 2,2,1,1,2,2,2 // RESPOSTA CERTA
+        c) 1,1,1,2,2,1,2
+        d) 1,2,2,2,1,1,1 
+        */
+        
+        /* QUESTÃO 3
+        Assinale a alternativa que contenha somente nome válidos de variáveis.
+        
+        a) índice, #pagina, contexto
+        b) nome1, sobrenome2, senha3 // RESPOSTA CERTA
+        c) 2-nome, sobrenome, endereco
+        d) 1-nome, 2-sobrenome, 3-senha  
+        */
+        
+        /* QUETÃO 4
+        Assinale a alternativa que completa corretamente a lacuna da afirmativa a seguir.
+        
+            São caracterizados como tipos ______ os dados numéricos positivos ou
+        negativos, excluindo-se destes qualquer fracionário.
+        
+        a) caracteres
+        b) lógicos 
+        c) inteiros // RESPOSTA CERTA
+        d) reais
+        */
+        
+        /* QUESTÃO 5
+        Considerando os tipos de dados, relacione as colunas e, a
+        seguir, assinale a alternativa com a sequência correta.
+        
+        (1) Inteiros        () 35; 0; -56
+        (2) Reais           () .F.; .V.
+        (3) Caracteres      () "Rua Brigadeiro Lyra"
+        (4) Lógicos         () -0,5; 1,8; -4
+        
+        a) 3, 1, 4, 2 
+        b) 2, 4, 3, 1
+        c) 1, 2, 3, 4
+        d) 1, 4, 3, 2 // RESPOSTA CERTA
+        
+        "TESTE DE MESA"
+        
+        (1) Inteiros        (1) 35; 0; -56
+        (2) Reais           (4) .F.; .V.
+        (3) Caracteres      (3) "Rua Brigadeiro Lyra"
+        (4) Lógicos         (2) -0,5; 1,8; -4
+        */
+    }
+    
+}
