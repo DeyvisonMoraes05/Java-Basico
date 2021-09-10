4764d351714e38a75888a68c5c87d310b1940159
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-09 23:13:31 -0400

    Aula 7D

---
+++ Aula07DExerciciosQuestoesTeoricas/src/aula07dexerciciosquestoesteoricas/Aula07DExerciciosQuestoesTeoricas.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula07dexerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula07DExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /* QUESTÃO 1
+        Considerando o trecho de código a seguir, construído
+        em Linguagem JAVA, marque a opção que preencha as
+        lacunas:
+        int n = 4;
+        System.out.println(“O valor da variável é “ + n++);
+        Executando as linhas acima, será exibida a mensagem
+        “O valor da variável é _____” e o valor final de n
+        após a execução será _____.
+        a) 3 e 4
+        b) 4 e 5 // RESPOSTA CERTA
+        c) 5 e 5
+        d) 3 e 3
+        */
+        
+        
+        /* QUESTÃO 2
+        Considerando o tratamento de tipos primitivos pelo
+        JAVA, analise o código a seguir e indique que valor
+        será impresso na tela:
+        int n1 = 2, n2 = 5;
+        double resultado = n2/n1;
+        System.out.print(resultado);
+        
+        a) 5.0
+        b) 2.0 // RESPOSTA CERTA
+        c) 2.5
+        d) 3.0
+        */
+        
+        /* QUESTÃO 3
+        Execute as três instruções JAVA a seguir e marque a
+        opção que contém o valor que será impresso na tela:
+        int v1 = 7, v2 = v1 % 2, v3 = 2;
+        v3 += v2;
+        System.out.println(v1 + " " + v2 + " " + v3);
+        
+        a) 7 2 4
+        b) 7 1 1
+        c) 7 1 3 // RESPOSTA CERTA
+        d) 7 0 2
+        
+        TESTE DE MESA
+        int v1 = 7, v2 = v1 % 2, v3 = 2;
+        V1 = 7
+        V2 = 7 TESTO DA DIVISÃO POR 2
+        V2 = 1
+        V3 = 2
+        v3 += v2;
+        V3 = V3 + V2
+        V3 = 2 + 1
+        V3 = 3
+        
+        */
+
+    }
+    
+}
