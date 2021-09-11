334fe8d8d756bab646739945f7e60739f75af682
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-10 22:44:28 -0400

    Aula 8A

---
+++ Aula08AOperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorternario;
+
+/**
+ *
+ * @author Usuario
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // OPERADOR TERNÁRIO
+        /*int n1 , n2, resultado;
+        n1 = 14;
+        n2 = 8;
+        resultado = (n1 > n2) ? n1 : n2;
+        System.out.println(resultado);*/
+        
+        // OPERADOR TERNÁRIO
+        /*int n1 , n2, resultado;
+        n1 = 14;
+        n2 = 18;
+        resultado = (n1 > n2) ? n1 + n2 : n1 - n2;
+        System.out.println(resultado);*/
+        
+        /*OPERADORES RELACIONAIS
+        >  | MAIOR QUE        | 5 > 2  | TRUE
+        <  | MENOR QUE        | 4 < 1  | FALSE
+        >= | MAIOR OU IGUAL A | 8 >= 3 | TRUE
+        <= | MENOR OU IGUAL A | 6 <= 6 | TRUE
+        == | IGUAL A          | 9 == 8 | FALSE
+        != | DIFERENTE DE     | 4 != 5 | TRUE
+        */
+        
+        /* COMPARANDO STRINGS */
+        String nome1 = "Deyvison";
+        String nome2 = "Deyvison";
+        String nome3 = new String("Deyvison");
+        System.out.println(nome3);
+        
+    }
+    
+}
