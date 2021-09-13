3176efce645d5f403a9b10b88f9a0de3fc13c2ac
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-12 23:25:18 -0400

    Aula 9D

---
+++ Aula09DExerciciosQuestoesTeoricas/src/aula09dexerciciosquestoesteoricas/Aula09DExerciciosQuestoesTeoricas.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula09dexerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula09DExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* QUESTÃO 1 
+        ANALISE O ALGORITMO ABAIXO.
+        LINHA 1 INÍCIO
+        LINHA 2     INTEIRO Y, X, DIV;
+        LINHA 3     LEIA(Y);
+        LINHA 4     X = Y % 7;
+        LINHA 5     SE X > 10;
+        LINHA 6         ENTÃO DIV = VERDADEIRO;
+        LINHA 7         SENÃO DIV = 10;
+        LINHA 8     FIMSE;
+        LINHA 9 FIM;
+        
+        ASSINALE A OPÇÃO QUE APRESENTA A LINHA QUE CONTÉM O ERRO NA EXECUÇÃO DO ALGORITMO.
+        (A) LINHA 2 INTEIRO Y, X, DIV.
+        (B) LINHA 4 X = Y % 7.
+        (C) LINHA 5 SE X > 10.
+        (D) LINHA 6 ENTÃO DIV = VERDADEIRO. (RESPOSTA CERTA), TIPOS DIFERENTES
+        (E) LINHA 7 SENÃO DIV = 10.
+        */
+        
+        /* QUESTÃO 1 
+        96- INDIQUE O RESULTADO DE "X" DO TRECHO DO PROGRAMA EM PORTUGUÊS ESTRUTURADO,
+        MOSTRADO ABAIXO. PARA AS SAÍDAS, CONSIDERE OS SEGUINTES VALORES:
+        A = 2; B = 3; C = 3, D = 4;
+            SE .NÃO. (D < 5) ENTÃO
+            // SE FALSE
+                X <- (A + B) * D
+            //  X <- (2 + 3) * 4 == 20   
+            SENÃO
+                X <- (C + A) * B
+            //  X <- (3 + 2) * 3 == 15
+            FIM_SE
+            ESCREVA X.
+            // ESCREVA 15.
+        
+        A) 15 RESPOSTA CERTA
+        B) 10
+        C) 25
+        D) 20
+        */
+        
+        /* QUESTÃO 3
+        Dada a estrutura representada abaixo:
+        int a = 5, b = 2;
+        String c;
+        if (a > b) {
+            c = “Primeiro é Maior”;
+        } else {
+            c = “Segundo é Maior”;
+        }
+        
+        Que linha substituiria a condição apresentada?
+        
+        a) c = (a < b)?”Primeiro é Maior”:”Segundo é Maior”;
+        b) c = (a <= b)?”Segundo é Maior”:”Primeiro é Maior”; // RESPOSTA CERTA
+        c) c = (a > b)?”Segundo é Maior”:”Primeiro é Maior”;
+        d) c = (a >= b)?”Primeiro é Maior”:”Segundo é Maior”;
+        */
+        
+        
+    }
+    
+}
