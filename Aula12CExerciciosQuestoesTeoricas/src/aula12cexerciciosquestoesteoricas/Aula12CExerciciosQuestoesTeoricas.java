df10e0a237081d344371d6497c59051b39ec7a31
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-15 23:21:10 -0400

    Aula 12C

---
+++ Aula12CExerciciosQuestoesTeoricas/src/aula12cexerciciosquestoesteoricas/Aula12CExerciciosQuestoesTeoricas.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12cexerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula12CExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        /* QUESTÃO 1
+        Considerando o trecho de Fluxograma representado e
+        mantendo a mesma estrutura de lógica de
+        programação, qual seria a sua transcrição válida
+        para Linguagem Java?
+                C <- C + 1
+                    S
+                    N C>10
+        
+        
+        a)  do { // RESPOSTA CERTA
+                c++;
+            } while(c<=10);
+        
+        b)  do {
+                c++;
+            } while(c>10);
+        
+        c)  while(c<=10) {
+                c++;
+            }
+        
+        d)  while(c>10) {
+                c++;
+            }
+        */
+        
+        /* QUESTÃO 2
+        Qual será o resultado impresso pelo trecho de
+        código escrito em Linguagem Java?
+        int c = 1;
+        do {
+            if (c % 5 != 0) System.out.print(c);
+            else break;
+            c+=1;
+        } while (c <= 10);
+        
+        a) 1 2 3 4 5 6 7 8 9 10
+        b) 1 2 3 4 6 7 8 9
+        c) 1 2 3 4 REPOSTA CERTA
+        d) Ocorrerá um erro de sintaxe
+        
+        
+        TESTE DE MESA 
+        C = 1; C += 1 C = 2; C += 1 C = 3; C += 1 C = 4; C += 1 C = 5;  
+        PRINTC = 1 2 3
+        1 % 5 = 1
+        2 % 5 = 2
+        3 % 5 = 3
+        4 % 5 = 4
+        5 % 5 = 0
+        BREAK;
+        */
+
+                
+    }
+    
+}
