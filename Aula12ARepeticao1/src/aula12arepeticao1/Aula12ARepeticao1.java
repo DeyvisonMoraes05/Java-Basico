2ec89ba4e2626fc48a730bfed147f16a2107e470
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-15 22:27:21 -0400

    Aula 12A

---
+++ Aula12ARepeticao1/src/aula12arepeticao1/Aula12ARepeticao1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12arepeticao1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula12ARepeticao1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL ("do" "while")
+        int cc = 0;
+        
+        do {
+            System.out.println("Cambalhota" + cc);
+            cc++;
+        } while (cc < 5);
+    }
+    
+}
