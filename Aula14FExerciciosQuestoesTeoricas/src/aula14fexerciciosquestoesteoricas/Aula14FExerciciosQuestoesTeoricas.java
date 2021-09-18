4980e495002793bb5d0c2ccba336f5e82c780ef4
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-17 23:22:50 -0400

    Aula 14F

---
+++ Aula14FExerciciosQuestoesTeoricas/src/aula14fexerciciosquestoesteoricas/Aula14FExerciciosQuestoesTeoricas.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula14fexerciciosquestoesteoricas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula14FExerciciosQuestoesTeoricas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* QUESTÃO 1
+        Conseiderando o techo de código Java representando a segui:
+        
+        int v[] = {2, 0, 3, 9};
+        v[v[2]] = v[v[1]];
+        for (int i:v) {
+            System.out.print(i);
+        }
+        
+        O que será impresso na tela?
+        
+        a) 2 0 3 2 RESPOSTA CERTA
+        b) 2 0 3 9
+        c) 2 0 0 9
+        d) 2 3 3 2
+        QUESTÃO BEM CONFUSA, REVISAR
+        */
+        
+        /* QUESTÃO 2
+        Execute o trecho Java a seguir e marque a opção que
+        contém o valor que será exibido na tela:*/
+        
+        /*
+        String s = "CURSOEMVIDEO"; // OBJETO "s" DA CLASSE "String"
+        char[] r = new char[12]; // VETOR DE  "char"
+        for (int c = s.length() - 1; c >= 0; c--) { // CONTAGEM REGRESSIVA
+            r[c] = s.charAt(c);
+        }
+        for (char l : r) { //FOREACH 
+            System.out.print(l);
+        }*/
+        
+        /*
+        a) OEDIVMEOSRUC
+        b) CURSOEMVIDEO // RESPOSTA CERTA
+        c) 01234567891011
+        d) 11109876543210
+        */
+
+    }
+    
+}
