abcea06a6a9504460f1087bc1959afe180612dcf
Deyvison Moraes <dtgnm.hef4@uea.edu.br>
2021-09-17 23:08:16 -0400

    Aula 14E

---
+++ Aula14EVetor5/src/aula14evetor5/Aula14EVetor5.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula14evetor5;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula14EVetor5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int v[] = new int[20];
+        Arrays.fill(v, 7); // PREENCHIMENTO AUTOMÁTICO DO VETOR COM UM NÚMERO QUALQUER
+        for (int valor: v) {
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
